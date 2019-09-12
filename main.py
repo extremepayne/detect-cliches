452df75407a99aeeca406550c34892af7a83fce5
extremepayne <harrison.payne@gmail.com>
2019-09-11 21:37:20 -0600

    Add function, etc.

--- main.py
+++ main.py
@@ -208,7 +208,48 @@ my_list = [
 ]
 my_text = ""
 
-for cliche in my_list:
+def ask(prompt, type_=None, min_=None, max_=None, range_=None):
+    """Get user input of a certain type, with range and min/max options."""
+    # comes from stack overflow:
+    """/questions/23294658/asking-the-user-for-input-until-they-give-a-valid-response/"""
+    if min_ is not None and max_ is not None and max_ < min_:
+        raise ValueError("min_ must be less than or equal to max_.")
+    while True:
+        ui = input(prompt)
+        if type_ is not None:
+            try:
+                ui = type_(ui)
+            except ValueError:
+                print("Input type must be {0}.".format(type_.__name__))
+                continue
+        if max_ is not None and ui > max_:
+            print("Input must be less than or equal to {0}.".format(max_))
+        elif min_ is not None and ui < min_:
+            print("Input must be greater than or equal to {0}.".format(min_))
+        elif range_ is not None and ui not in range_:
+            if isinstance(range_, range):
+                template = "Input must be between {0.start} and {0.stop}."
+                print(template.format(range_))
+            else:
+                template = "Input must be {0}."
+                if len(range_) == 1:
+                    print(template.format(*range_))
+                else:
+                    print(
+                        template.format(
+                            " or ".join(
+                                (
+                                    ", ".join(map(str, range_[:-1])),
+                                    str(range_[-1]),
+                                )
+                            )
+                        )
+                    )
+        else:
+            return ui
+
+lowercase_text = my_text.lower()
+for cliche in clichelist:
   if cliche in my_text:
     print(cliche)
   else:
